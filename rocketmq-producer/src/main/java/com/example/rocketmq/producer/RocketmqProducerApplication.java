af1cde2d8c76931a524317d01cab01fa23c98764
HSJ <hesuijin@qq.com>
2021-02-28 14:23:12 +0800

    新增rocketmq - producer 与 rocket-consumer模块

---
+++ rocketmq-producer/src/main/java/com/example/rocketmq/producer/RocketmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocketmq.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerApplication.class, args);
+    }
+
+}
