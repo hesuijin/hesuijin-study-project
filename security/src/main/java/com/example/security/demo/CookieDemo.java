b81917fb551b6e9db8ba3950e12d012be65ab51d
hesuijin <hesuijin@qq.com>
2021-03-03 11:07:10 +0800

    Cookie测试

--- security/src/main/java/com/example/security/demo/CookieDemo.java
+++ security/src/main/java/com/example/security/demo/CookieDemo.java
@@ -24,24 +24,6 @@ import java.util.stream.Collectors;
 public class CookieDemo {
 
     /**
-     * 设置cookie返回给客户端
-     * @param response
-     * @return
-     */
-    @GetMapping("/change-username")
-    public String setCookie(HttpServletResponse response) throws InterruptedException {
-        // 创建⼀个 cookie
-        Cookie cookie = new Cookie("username", "HeSuiJin");
-        //设置 cookie过期时间
-        // expires in 7 days
-        cookie.setMaxAge(7 * 24 * 60 * 60);
-        //添加到 response 中
-        response.addCookie(cookie);
-        Thread.sleep(10000L);
-        return "Username is changed!";
-    }
-
-    /**
      * 使⽤Spring框架提供的 @CookieValue 注解获取特定的 cookie的值
      * @param username
      * @return
@@ -52,6 +34,23 @@ public class CookieDemo {
         return "Hey! My username is " + username;
     }
 
+    /**
+     * 设置cookie返回给客户端
+     * @param response
+     * @return
+     */
+    @GetMapping("/change-username")
+    public String setCookie(HttpServletResponse response) throws InterruptedException {
+        // 创建⼀个 cookie
+        Cookie cookie = new Cookie("username", "HeSuiJin");
+//        Cookie cookie = new Cookie("username", null);
+        //设置 cookie过期时间
+        // expires in 7 days
+        cookie.setMaxAge(7 * 24 * 60 * 60);
+        //添加到 response 中
+        response.addCookie(cookie);
+        return "Username is changed!";
+    }
 
     /**
      * 读取所有的 Cookie 值
