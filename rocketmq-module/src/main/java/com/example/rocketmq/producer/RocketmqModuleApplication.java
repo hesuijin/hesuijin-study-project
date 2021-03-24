ce68b8afab61f7e3129004a94d6398d438cccf7e
HSJ <hesuijin@qq.com>
2021-03-24 19:05:16 +0800

    删除 rocket-consumer模块

--- rocketmq-module/src/main/java/com/example/rocketmq/producer/RocketmqModuleApplication.java
+++ rocketmq-module/src/main/java/com/example/rocketmq/producer/RocketmqModuleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RocketmqProducerApplication {
+public class RocketmqModuleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RocketmqProducerApplication.class, args);
+        SpringApplication.run(RocketmqModuleApplication.class, args);
     }
 
 }
