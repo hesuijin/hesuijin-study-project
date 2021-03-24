0fee1c5035f6004e76a0da910dc21b009bdad19c
HSJ <hesuijin@qq.com>
2021-03-24 18:55:27 +0800

    新增 spring security 源码阅读

---
+++ spring-security-jwt-module/src/main/java/com/example/security/a_source_code/SampleAuthenticationManager.java
@@ -0,0 +1,36 @@
+package com.example.security.a_source_code;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author hesuijin
+ * @Description
+ * @Param
+ * @Return
+ * @Date 2021/3/34
+ */
+class SampleAuthenticationManager implements AuthenticationManager {
+    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+
+    static {
+        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+        if (auth.getName().equals(auth.getCredentials())) {
+            return new UsernamePasswordAuthenticationToken(auth.getName(),
+                    auth.getCredentials(), AUTHORITIES);
+        }
+        throw new BadCredentialsException("Bad Credentials");
+    }
+}
