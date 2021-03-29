0f6fadd6918506040bf2e1640705e59bdad40a40
HSJ <hesuijin@qq.com>
2021-03-29 13:02:01 +0800

    新增Socket demo

---
+++ netty-module/src/main/java/com/example/demo/socket/SocketMessage.java
@@ -0,0 +1,16 @@
+package com.example.demo.socket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author HeSuiJin
+ * @Date 2021/3/29
+ */
+@Data
+@AllArgsConstructor
+public class SocketMessage {
+
+    private String content;
+}
