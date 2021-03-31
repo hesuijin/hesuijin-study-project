89198360dbeec54bfda0001764720fe288eee26d
HSJ <hesuijin@qq.com>
2021-03-31 15:14:38 +0800

    Redis整合

--- redis-module/src/main/java/com/example/demo/controller/RedisUtilTestController.java
+++ redis-module/src/main/java/com/example/demo/controller/RedisUtilTestController.java
@@ -22,25 +22,58 @@ public class RedisUtilTestController {
     private RedisStringUtil redisStringUtil;
 
     @GetMapping("setDomain")
-    public String addDomainTest(){
+    public String setDomainTest(){
         User user = new User();
         user.setId(1L);
         user.setName("hesuijin");
         user.setAge(18);
 
-        redisStringUtil.setObject("user1",user);
+        redisStringUtil.setObject("user",user);
         return "set success";
     }
 
     @GetMapping("getDomain")
     public String getDomainTest(){
+        User user = redisStringUtil.getObject("user",User.class);
 
-        User user = redisStringUtil.getObject("user1",User.class);
+//         {"age":18,"id":1,"name":"hesuijin"}
+        return JSONObject.toJSONString(user);
 
 //        User(id=1, name=hesuijin, age=18)
-        return user.toString();
+//        return user.toString();
+    }
 
-//        {"age":18,"id":1,"name":"hesuijin"}
-//        return JSONObject.toJSONString(user);
+    /**
+     *  使用分号 : 区分多级
+     * @return
+     */
+    @GetMapping("setMulDomain")
+    public String setMulDomainTest(){
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setName("hesuijin");
+        user1.setAge(18);
+
+        User user2 = new User();
+        user2.setId(2L);
+        user2.setName("HSJ");
+        user2.setAge(18);
+
+        redisStringUtil.setObject("redisUtil:user1",user1);
+        redisStringUtil.setObject("redisUtil:user2",user1);
+        return "set success";
+    }
+
+    /**
+     * 需要分开获取  不能直接获取整个包
+     * @return
+     */
+    @GetMapping("getMulDomain")
+    public String getMulDomainTest(){
+        String returnString1 = redisStringUtil.getString("redisUtil:user1");
+        String returnString2 = redisStringUtil.getString("redisUtil:user2");
+
+        return  "返回1:"+returnString1+ "返回2："+returnString2;
+
     }
 }
