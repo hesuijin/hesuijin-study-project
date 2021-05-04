21db99d4c2bb5734404a94875a5de5130aab8e7f
HSJ <hesuijin@qq.com>
2021-05-04 19:16:17 +0800

    spring Security jwt 注释源码阅读

--- spring-security-jwt-module/src/main/java/com/example/security/source_code/AuthenticationExample.java
+++ spring-security-jwt-module/src/main/java/com/example/security/source_code/AuthenticationExample.java
@@ -1,45 +1,45 @@
-package com.example.security.source_code;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-/**
- * @Author hesuijin
- * @Description
- * @Param
- * @Return
- * @Date 2021/3/34
- */
-public class AuthenticationExample {
-
-    private static AuthenticationManager am = new SampleAuthenticationManager();
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
-        while (true) {
-            System.out.println("Please enter your username:");
-            String name = in.readLine();
-            System.out.println("Please enter your password:");
-            String password = in.readLine();
-            try {
-                //name 为 principal   password 为 credentials
-                Authentication request = new UsernamePasswordAuthenticationToken(name, password);
-                Authentication result = am.authenticate(request);
-                SecurityContextHolder.getContext().setAuthentication(result);
-                break;
-            } catch (AuthenticationException e) {
-                System.out.println("Authentication failed:" + e.getMessage());
-            }
-        }
-        System.out.println("Successfully authenticated. Security context contains:" +
-                SecurityContextHolder.getContext().getAuthentication());
-    }
-}
+//package com.example.security.source_code;
+//
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.ProviderManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//
+//import java.io.BufferedReader;
+//import java.io.InputStreamReader;
+//
+///**
+// * @Author hesuijin
+// * @Description
+// * @Param
+// * @Return
+// * @Date 2021/3/34
+// */
+//public class AuthenticationExample {
+//
+//    private static AuthenticationManager am = new SampleAuthenticationManager();
+//
+//    public static void main(String[] args) throws Exception {
+//        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//
+//        while (true) {
+//            System.out.println("Please enter your username:");
+//            String name = in.readLine();
+//            System.out.println("Please enter your password:");
+//            String password = in.readLine();
+//            try {
+//                //name 为 principal   password 为 credentials
+//                Authentication request = new UsernamePasswordAuthenticationToken(name, password);
+//                Authentication result = am.authenticate(request);
+//                SecurityContextHolder.getContext().setAuthentication(result);
+//                break;
+//            } catch (AuthenticationException e) {
+//                System.out.println("Authentication failed:" + e.getMessage());
+//            }
+//        }
+//        System.out.println("Successfully authenticated. Security context contains:" +
+//                SecurityContextHolder.getContext().getAuthentication());
+//    }
+//}
--- spring-security-jwt-module/src/main/java/com/example/security/source_code/SampleAuthenticationManager.java
+++ spring-security-jwt-module/src/main/java/com/example/security/source_code/SampleAuthenticationManager.java
@@ -1,37 +1,37 @@
-package com.example.security.source_code;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/**
- * @Author hesuijin
- * @Description
- * @Param
- * @Return
- * @Date 2021/3/34
- */
-class SampleAuthenticationManager implements AuthenticationManager {
-    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
-
-    static {
-        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
-    }
-
-    @Override
-    public Authentication authenticate(Authentication auth) throws AuthenticationException {
-        //auth.getName()  就是调用 Principal中getName() 的方法  获取名称
-        if (auth.getName().equals(auth.getCredentials())) {
-            return new UsernamePasswordAuthenticationToken(auth.getName(),
-                    auth.getCredentials(), AUTHORITIES);
-        }
-        throw new BadCredentialsException("Bad Credentials");
-    }
-}
+//package com.example.security.source_code;
+//
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.BadCredentialsException;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * @Author hesuijin
+// * @Description
+// * @Param
+// * @Return
+// * @Date 2021/3/34
+// */
+//class SampleAuthenticationManager implements AuthenticationManager {
+//    static final List<GrantedAuthority> AUTHORITIES = new ArrayList<GrantedAuthority>();
+//
+//    static {
+//        AUTHORITIES.add(new SimpleGrantedAuthority("ROLE_USER"));
+//    }
+//
+//    @Override
+//    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+//        //auth.getName()  就是调用 Principal中getName() 的方法  获取名称
+//        if (auth.getName().equals(auth.getCredentials())) {
+//            return new UsernamePasswordAuthenticationToken(auth.getName(),
+//                    auth.getCredentials(), AUTHORITIES);
+//        }
+//        throw new BadCredentialsException("Bad Credentials");
+//    }
+//}
