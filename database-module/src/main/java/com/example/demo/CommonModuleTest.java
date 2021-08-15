92dfdd722c028c58418407e6272ba2b4ca5817da
HSJ <hesuijin@qq.com>
2021-08-15 23:22:22 +0800

    打包测试

---
+++ database-module/src/main/java/com/example/demo/CommonModuleTest.java
@@ -0,0 +1,22 @@
+import com.example.demo.dataCreataTion.CreateBusinessList;
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+
+//import com.example.demo.dataCreataTion.CreateBusinessList;
+
+/**
+ * @Description:
+ * @Author HeSuiJin
+ * @Date 2021/8/15
+ */
+public class CommonModuleTest {
+
+    public static void main(String[] args) {
+//        CreateBusinessList
+
+//        CreateBusinessList
+
+//        CreateBusinessList createBusinessList = new CreateBusinessList();
+
+    }
+
+}
